c27ac2c71ca6b512e0ee9bd147c98d96c94050e9
Feykich <80963318+Feykich@users.noreply.github.com>
2024-06-09 19:41:04 +0300

    Minor optimization

--- WeaponKnifeExplosion.cs
+++ WeaponKnifeExplosion.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.Json.Serialization;
+using System.Text.Json.Serialization;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 
@@ -13,7 +13,7 @@ public class Settings : BasePluginConfig
 public class WeaponKnifeExplosion : BasePlugin, IPluginConfig<Settings>
 {
     public override string ModuleName => "[ANY] Weapon/Knife Explosion";
-    public override string ModuleVersion => "1.0.0";
+    public override string ModuleVersion => "1.0.1";
     public override string ModuleAuthor => "Feykich";
 
     public Settings Config { get; set; } = null!;
@@ -27,62 +27,35 @@ public class WeaponKnifeExplosion : BasePlugin, IPluginConfig<Settings>
         Config = config;
     }
 
-    //private readonly Dictionary<CCSPlayerController, bool> bParticleCheck = [];
-
     public override void Load(bool hotReload)
     {    
         Console.WriteLine("[Weapon/Knife Explosion] Plugin is Loaded!");
-
-        /*
-        RegisterEventHandler<EventPlayerSpawn>((@event, info) => 
-        {
-            bParticleCheck[@event.Userid] = false;
-            return HookResult.Continue;
-        });
-        */
   
         RegisterEventHandler<EventPlayerHurt>(OnEventPlayerHurt);
     }
 
     private HookResult OnEventPlayerHurt(EventPlayerHurt @event, GameEventInfo _)
     {
-        var client = @event.Userid;
-        var attacker = @event.Attacker;
-        var weapon = @event.Attacker.PlayerPawn.Value!.WeaponServices!.ActiveWeapon.Value!.DesignerName;
+        var client =    @event.Userid;
+        var attacker =  @event.Attacker;
+        var weapon =    @event.Attacker.PlayerPawn.Value!.WeaponServices!.ActiveWeapon.Value!.DesignerName;
 
         if(!client.IsValid || !attacker.IsValid) 
         {
+            Console.WriteLine("[Weapon/Knife Explosion] Attacker/Victim is not found.");
             return 0;
         }
 
-        client.PrintToChat($"[Weapon/Knife Explosion] You're gonna explode in {Config.TimerValue} seconds!");
+        client.PrintToChat($"[Weapon/Knife Explosion] You're gonna explode in {Config.TimerValue} second(s)!");
+
         AddTimer(Config.TimerValue, () =>
         {
-            if(Config.KnifeEnable == true && weapon == "weapon_knife" && client.PawnIsAlive)
-            {
-                OnKillPlayer(client);
-                OnParticleExplosion(client);
-            }
-            if(Config.KnifeEnable == false && client.PawnIsAlive)
-            {
-                OnKillPlayer(client);
-                OnParticleExplosion(client);
-            } 
+            OnKillPlayer(client, weapon);
         });
         return HookResult.Continue;
     }
 
-    private static void OnKillPlayer(CCSPlayerController? client)
-    {
-        if(client == null)
-        {
-            Console.WriteLine("[Knife Explosion] Null");
-            return;
-        }
-        client.CommitSuicide(true, true);
-    }
-
-    private void OnParticleExplosion(CCSPlayerController? client)
+    private void OnKillPlayer(CCSPlayerController? client, string weapon)
     {
         CParticleSystem? entity = Utilities.CreateEntityByName<CParticleSystem>("info_particle_system");
 
@@ -90,9 +63,15 @@ public class WeaponKnifeExplosion : BasePlugin, IPluginConfig<Settings>
         var vAngle =    client!.PlayerPawn.Value!.AbsRotation!;
         var sVec =      client!.PlayerPawn.Value!.AbsVelocity!;
 
+        if(!client.IsValid)
+        {
+            Console.WriteLine("[Weapon/Knife Explosion] Client not found.");
+            return;
+        }
+        
         if(entity != null)
         {      
-            entity.EffectName = "particles/explosions_fx/explosion_basic.vpcf";
+            entity.EffectName = "particles/explosions_fx/explosion_basic.vpcf"; // HE Grenade particle
             entity.DispatchSpawn();
             entity.Teleport(pOrigin, vAngle, sVec);
             entity.AcceptInput("Start");
@@ -101,5 +80,15 @@ public class WeaponKnifeExplosion : BasePlugin, IPluginConfig<Settings>
                 entity.AcceptInput("Kill");
             });
         }
+        if(Config.KnifeEnable == true && weapon == "weapon_knife" && client.PawnIsAlive)
+        {
+            client.CommitSuicide(true, true);
+            return;
+        }
+        if(client.PawnIsAlive)
+        {
+            client.CommitSuicide(true, true); // Allows weapons detonate a player by hit. If boolean "KnifeEnable" is false
+        }
+        return;
     }
 }
